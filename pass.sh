a11229e6ac95df3170371dae881646f35f5a3f2b
Keshav Gupta <140381004+officialkeshav@users.noreply.github.com>
2023-09-25 16:02:19 +0530

    pass.sh

--- pass.sh
+++ pass.sh
@@ -1,3 +1,27 @@
+echo -e '\e[95m               
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e '\e[94mX  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
+ X# \.X        @#%,.@                @#%,.@        
+                @#%,.@   BOMBE      @#%,.@          
+                  @#%,.@          @#%,.@            
+                     @#%,.@      @#%,.@             
+                       @#%.,@  @#%,.@              
+                        keshav Gupta
+               INSTAGRAM:- official_.keshav'
+                echo -e '\e[93m CODED BY KESHAV'
+
+echo  -ne "\e[92m Enter Your number :-" |lolcat
 figlet bomber |lolcat
 echo -e "\e[91m coded by keshav"
 echo  -ne "enter Your number :" |lolcat
